aa4be75524462ad9ee42df0e512da3388e9bee76
J <41439659+daeds1dog@users.noreply.github.com>
2021-03-27 08:14:05 +0300

    Format

--- patches/special_abilities.hpp
+++ patches/special_abilities.hpp
@@ -5,14 +5,14 @@
 
 #ifndef STEAM
 #define formations_fix_start 0x8d4964 - IMAGE_BASE
-#define formations_fix_end 0x8d4970 - IMAGE_BASE
-#define swimming_fix_start 0x8d43c1 - IMAGE_BASE
-#define swimming_fix_end 0x8d43c9 - IMAGE_BASE
+#define formations_fix_end   0x8d4970 - IMAGE_BASE
+#define swimming_fix_start   0x8d43c1 - IMAGE_BASE
+#define swimming_fix_end     0x8d43c9 - IMAGE_BASE
 #else
 #define formations_fix_start 0x91a30f - IMAGE_BASE
-#define formations_fix_end 0x91a32c - IMAGE_BASE
-#define swimming_fix_start 0x91931 - IMAGE_BASE
-#define swimming_fix_end 0x91938 - IMAGE_BASE
+#define formations_fix_end   0x91a32c - IMAGE_BASE
+#define swimming_fix_start   0x91931 - IMAGE_BASE
+#define swimming_fix_end     0x91938 - IMAGE_BASE
 #endif
 
 class special_abilities {
@@ -20,6 +20,7 @@ class special_abilities {
 
     static void __declspec(naked) enable_bi_formations();
     static void __declspec(naked) enable_swimming();
+
   public:
     static void patch();
 };
