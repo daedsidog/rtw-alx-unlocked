bafb1f54dc56da24412f984a9128d8cf03012ed1
J <41439659+daeds1dog@users.noreply.github.com>
2021-04-15 15:44:09 +0300

    Add hording functionality

--- patches/special_abilities.cpp
+++ patches/special_abilities.cpp
@@ -76,7 +76,186 @@ void __declspec(naked) special_abilities::enable_hording_ability() {
         ret
     }
 }
+char *    horde_min_units;
+char *    horde_max_units;
+char *    horde_max_units_reduction_every_horde;
+char *    horde_unit_per_settlement_population;
+char *    horde_min_named_characters;
+char *    horde_max_percent_army_stack;
+char *    horde_disband_percent_on_settlement_cap;
+char *    horde_unit;
+uintptr_t hording_init_end;
+uintptr_t hording_enable_flag;
+uintptr_t some_parsing_func;
+uintptr_t strparse_func;
 
+void special_abilities::enable_hording_functionality() {
+    asm {
+        add esp, 0x4
+        case_horde_min_units:
+		push horde_min_units
+        lea ecx, [esp + 0x170]
+        lea eax, strparse_func_ret_1
+		push eax
+		push strparse_func
+		ret
+		strparse_func_ret_1:
+        movzx eax, al
+        test eax, eax
+        jz case_horde_max_units
+		lea ecx, [esp + 0x16c]
+		lea eax, some_parsing_func_ret_1
+		push eax
+		push some_parsing_func
+		ret
+		some_parsing_func_ret_1:
+		case_horde_max_units:
+		push horde_max_units
+        lea ecx, [esp + 0x170]
+        lea eax, strparse_func_ret_2
+		push eax
+		push strparse_func
+		ret
+		strparse_func_ret_2:
+        movzx eax, al
+        test eax, eax
+        jz case_horde_max_units_reduction_every_horde
+		lea ecx, [esp + 0x16c]
+		lea eax, some_parsing_func_ret_2
+		push eax
+		push some_parsing_func
+		ret
+		some_parsing_func_ret_2:
+		case_horde_max_units_reduction_every_horde:
+		push horde_max_units_reduction_every_horde
+        lea ecx, [esp + 0x170]
+        lea eax, strparse_func_ret_3
+		push eax
+		push strparse_func
+		ret
+		strparse_func_ret_3:
+        movzx eax, al
+        test eax, eax
+        jz case_horde_unit_per_settlement_population
+		lea ecx, [esp + 0x16c]
+		lea eax, some_parsing_func_ret_3
+		push eax
+		push some_parsing_func
+		ret
+		some_parsing_func_ret_3:
+		case_horde_unit_per_settlement_population:
+		push horde_unit_per_settlement_population
+        lea ecx, [esp + 0x170]
+        lea eax, strparse_func_ret_4
+		push eax
+		push strparse_func
+		ret
+		strparse_func_ret_4:
+        movzx eax, al
+        test eax, eax
+        jz case_horde_min_named_characters
+		lea ecx, [esp + 0x16c]
+		lea eax, some_parsing_func_ret_4
+		push eax
+		push some_parsing_func
+		ret
+		some_parsing_func_ret_4:
+		case_horde_min_named_characters:
+		push horde_min_named_characters
+        lea ecx, [esp + 0x170]
+        lea eax, strparse_func_ret_5
+		push eax
+		push strparse_func
+		ret
+		strparse_func_ret_5:
+        movzx eax, al
+        test eax, eax
+        jz case_horde_max_percent_army_stack
+		lea ecx, [esp + 0x16c]
+		lea eax, some_parsing_func_ret_5
+		push eax
+		push some_parsing_func
+		ret
+		some_parsing_func_ret_5:
+		case_horde_max_percent_army_stack:
+		push horde_max_percent_army_stack
+        lea ecx, [esp + 0x170]
+        lea eax, strparse_func_ret_6
+		push eax
+		push strparse_func
+		ret
+		strparse_func_ret_6:
+        movzx eax, al
+        test eax, eax
+        jz case_horde_disband_percent_on_settlement_cap
+		lea ecx, [esp + 0x16c]
+		lea eax, some_parsing_func_ret_6
+		push eax
+		push some_parsing_func
+		ret
+		some_parsing_func_ret_6:
+		case_horde_disband_percent_on_settlement_cap:
+		push horde_disband_percent_on_settlement_cap
+        lea ecx, [esp + 0x170]
+        lea eax, strparse_func_ret_7
+		push eax
+		push strparse_func
+		ret
+		strparse_func_ret_7:
+        movzx eax, al
+        test eax, eax
+        jz case_horde_unit
+        case_special_horde_disband_percent_on_settlement_cap:
+		lea ecx, [esp + 0x16c]
+		lea eax, some_parsing_func_ret_7
+		push eax
+		push some_parsing_func
+		ret
+		some_parsing_func_ret_7:
+        push horde_disband_percent_on_settlement_cap
+        lea ecx, [esp + 0x170]
+        lea eax, special_ret
+        push eax
+        push strparse_func
+        ret
+        special_ret:
+        movzx eax, al
+        test eax, eax
+        jnz case_special_horde_disband_percent_on_settlement_cap
+		case_horde_unit:
+		push horde_unit
+        lea ecx, [esp + 0x170]
+        lea eax, strparse_func_ret_8
+		push eax
+		push strparse_func
+		ret
+		strparse_func_ret_8:
+        movzx eax, al
+        test eax, eax
+        jz end
+        case_special_horde_unit:
+		lea ecx, [esp + 0x16c]
+		lea eax, some_parsing_func_ret_8
+		push eax
+		push some_parsing_func
+		ret
+		some_parsing_func_ret_8:
+        mov byte ptr [esp + 0x155], 0x1
+        push horde_unit
+        lea ecx, [esp + 0x170]
+        lea eax, strparse_func_ret_9
+        push eax
+        push strparse_func
+        ret
+        strparse_func_ret_9:
+        movzx eax, al
+        test eax, eax
+        jnz case_special_horde_unit
+		end:
+        push hording_init_end
+        ret
+    }
+}
 void special_abilities::patch() {
     AHI::init();
     std::cout << "Enabling shield_wall and schiltrom formations..."
@@ -88,6 +267,12 @@ void special_abilities::patch() {
 #ifndef STEAM
     abilities_ifchain_end =
         AHI::get_abs_addr(IMAGE_BASE, abilities_ifchain_end_vaddr);
+    hording_enable_flag =
+        AHI::get_abs_addr(IMAGE_BASE, hording_enable_flag_vaddr);
+    some_parsing_func = AHI::get_abs_addr(IMAGE_BASE, some_parsing_func_vaddr);
+    strparse_func     = AHI::get_abs_addr(IMAGE_BASE, strparse_func_vaddr);
+    hording_init_end  = AHI::get_abs_addr(
+        IMAGE_BASE, hording_functionality_enable_fix_end_vaddr);
 #else
     abilities_ifchain_end =
         AHI::get_abs_addr(IMAGE_BASE, abilities_ifchain_end_vaddr);
@@ -100,9 +285,26 @@ void special_abilities::patch() {
         AHI::get_offset(IMAGE_BASE, swimming_ability_enable_fix_start_vaddr),
         AHI::get_offset(IMAGE_BASE, swimming_ability_enable_fix_end_vaddr),
         (LPVOID)enable_swimming_ability);
-    std::cout << "Enabling hording ability..." << std::endl;
+    std::cout << "Enabling hording ability & functionality..." << std::endl;
+#define defstr(x, y) x = (char *)AHI::get_abs_addr(IMAGE_BASE, y);
+    defstr(horde_min_units, horde_min_units_vaddr);
+    defstr(horde_max_units, horde_max_units_vaddr);
+    defstr(horde_max_units_reduction_every_horde,
+           horde_max_units_reduction_every_horde_vaddr);
+    defstr(horde_unit_per_settlement_population,
+           horde_unit_per_settlement_population_vaddr);
+    defstr(horde_min_named_characters, horde_min_named_characters_vaddr);
+    defstr(horde_max_percent_army_stack, horde_max_percent_army_stack_vaddr);
+    defstr(horde_disband_percent_on_settlement_cap,
+           horde_disband_percent_on_settlement_cap_vaddr);
+    defstr(horde_unit, horde_unit_vaddr);
     AHI::inject_func(
         AHI::get_offset(IMAGE_BASE, hording_ability_enable_fix_start_vaddr),
         AHI::get_offset(IMAGE_BASE, hording_ability_enable_fix_end_vaddr),
         (LPVOID)enable_hording_ability);
+    AHI::inject_func(
+        AHI::get_offset(IMAGE_BASE,
+                        hording_functionality_enable_fix_start_vaddr),
+        AHI::get_offset(IMAGE_BASE, hording_functionality_enable_fix_end_vaddr),
+        (LPVOID)enable_hording_functionality);
 }
--- patches/special_abilities.hpp
+++ patches/special_abilities.hpp
@@ -3,13 +3,28 @@
 #define IMAGE_BASE 0x400000
 
 #ifndef STEAM
-#define abilities_ifchain_end_vaddr             0x8d479d
-#define formations_fix_start_vaddr              0x8d4964
-#define formations_fix_end_vaddr                0x8d4970
-#define swimming_ability_enable_fix_start_vaddr 0x8d43c1
-#define swimming_ability_enable_fix_end_vaddr   0x8d43c9
-#define hording_ability_enable_fix_start_vaddr  0x8d43d8
-#define hording_ability_enable_fix_end_vaddr    0x8d43e0
+#define abilities_ifchain_end_vaddr                  0x8d479d
+#define formations_fix_start_vaddr                   0x8d4964
+#define formations_fix_end_vaddr                     0x8d4970
+#define swimming_ability_enable_fix_start_vaddr      0x8d43c1
+#define swimming_ability_enable_fix_end_vaddr        0x8d43c9
+#define hording_ability_enable_fix_start_vaddr       0x8d43d8
+#define hording_ability_enable_fix_end_vaddr         0x8d43e0
+#define hording_enable_flag_vaddr                    0x103ba34
+#define hording_functionality_enable_fix_start_vaddr 0x8ac6b4
+#define hording_functionality_enable_fix_end_vaddr   0x8ac794
+// #define hording_functionality_enable_fix_start_vaddr 0x8ac6b4
+// #define hording_functionality_enable_fix_end_vaddr   0x8ac6b9
+#define some_parsing_func_vaddr                       0xdbe9b4
+#define strparse_func_vaddr                           0xdbedf0
+#define horde_min_units_vaddr                         0x29e56a0
+#define horde_max_units_vaddr                         0x29e56b0
+#define horde_max_units_reduction_every_horde_vaddr   0x29e56c0
+#define horde_unit_per_settlement_population_vaddr    0x29e56e8
+#define horde_min_named_characters_vaddr              0x29e5710
+#define horde_max_percent_army_stack_vaddr            0x29e572c
+#define horde_disband_percent_on_settlement_cap_vaddr 0x29e574c
+#define horde_unit_vaddr                              0x29e5778
 #else
 #define abilities_ifchain_end_vaddr             0x919fd7
 #define swimming_ifchain_end_vaddr              0x919a3d
@@ -28,6 +43,7 @@ class special_abilities {
     static void __declspec(naked) enable_bi_formations();
     static void __declspec(naked) enable_swimming_ability();
     static void __declspec(naked) enable_hording_ability();
+    static void __declspec(naked) enable_hording_functionality();
 
   public:
     static void patch();
